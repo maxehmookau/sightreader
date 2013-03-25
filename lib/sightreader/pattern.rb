5159978da16f1aba074cf45ac242061fa9c95d92
Max Woolf <maximus@zingysaturn.co.uk>
2013-03-25 20:47:23 +0000

    tests!

--- lib/sightreader/pattern.rb
+++ lib/sightreader/pattern.rb
@@ -11,8 +11,19 @@ module Sightreader
     end
 
     def bars=(number_of_bars)
-      @rhythm = [('-' * 16) * number_of_bars]
+      number_of_bars*16.times do
+        @rhythm << '-'
+      end
       @bars = number_of_bars
+    end
+
+    def toggle_beat(options = {})
+      beat_to_toggle = (16 * (options[:bar] - 1) + options[:beat] - 1)
+      if @rhythm[beat_to_toggle] == '-'
+        @rhythm[beat_to_toggle] = 'x'
+      else
+        @rhythm[beat_to_toggle] = '-'
+      end
     end
 
   end
--- spec/pattern_spec.rb
+++ spec/pattern_spec.rb
@@ -19,13 +19,39 @@ describe '#bars=' do
   it 'should fill the @rhythm Array with the correct number of dashes' do
     pattern = Sightreader::Pattern.new
     pattern.bars = 1
-    pattern.rhythm.should eq(['----------------'])
+    pattern.rhythm.should eq(['-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-'])
   end
 
   it 'should set @bars' do
     pattern = Sightreader::Pattern.new
     pattern.bars = 23
     pattern.bars.should eq 23
+  end
+
+end
+
+describe '#toggle_beat' do
+
+  it 'should turn on a beat when off' do
+    pattern = Sightreader::Pattern.new
+    pattern.bars = 1
+    pattern.toggle_beat(bar: 1, beat: 1)
+    pattern.rhythm.should eq(['x','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-'])
+  end
+
+  it 'should turn on a beat when off (2)' do
+    pattern = Sightreader::Pattern.new
+    pattern.bars = 1
+    pattern.toggle_beat(bar: 1, beat: 4)
+    pattern.rhythm.should eq(['-', '-', '-', 'x', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'])
+  end
+
+  it 'should turn on a beat when off (3)' do
+    pattern = Sightreader::Pattern.new
+    pattern.bars = 1
+    pattern.toggle_beat(bar: 1, beat: 4)
+    pattern.toggle_beat(bar: 1, beat: 10)
+    pattern.rhythm.should eq(['-', '-', '-','x', '-','-', '-', '-', '-', 'x', '-', '-', '-', '-', '-', '-'])
   end
 
 end
